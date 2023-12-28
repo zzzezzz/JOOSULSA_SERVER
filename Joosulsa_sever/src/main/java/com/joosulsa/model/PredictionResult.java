a629cd3516e377058bd100375e3f3e8826de465f
jhch1116 <jhch1116@naver.com>
2023-12-28 16:38:51 +0900

    플라스크 정보 받기

---
+++ Joosulsa_sever/src/main/java/com/joosulsa/model/PredictionResult.java
@@ -0,0 +1,10 @@
+package com.joosulsa.model;
+
+public class PredictionResult {
+	private String className;
+	
+	public PredictionResult(String className) {
+		this.className = className;
+		
+	}
+}
