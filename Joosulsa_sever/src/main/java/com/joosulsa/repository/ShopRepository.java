8401f1dc1ec0b9cc9f8ab807d87f3c47746e8d8a
fdsv8596 <hyunjun01070@gmail.com>
2024-01-09 16:30:50 +0900

    이것도 날라가면 탈주함

--- Joosulsa_sever/src/main/java/com/joosulsa/repository/ShopRepository.java
+++ Joosulsa_sever/src/main/java/com/joosulsa/repository/ShopRepository.java
@@ -1,11 +1,17 @@
 package com.joosulsa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.joosulsa.entity.Tb_Product;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Tb_Product, String>{	 
+	
 
+	
+	
 }
