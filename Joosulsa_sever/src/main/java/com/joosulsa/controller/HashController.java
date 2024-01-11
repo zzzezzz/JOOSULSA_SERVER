0b6cd7e3e1cdee33190fcd40bec56c64edaa8e4c
fpdls2789 <fpdls2789@gmail.com>
2024-01-11 09:12:59 +0900

    해시태그 작업

--- Joosulsa_sever/src/main/java/com/joosulsa/controller/HashController.java
+++ Joosulsa_sever/src/main/java/com/joosulsa/controller/HashController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joosulsa.dto.RecycleDTO.cateData;
+import com.joosulsa.entity.Tb_Recycling;
 import com.joosulsa.repository.SearchRepository;
 
 @RestController
@@ -22,33 +24,29 @@ public class HashController {
 	@Autowired
 	private SearchRepository searchRepo;
 	
+	// 해시태그 버튼 클릭 시 처리해주는 곳
 	@PostMapping("/hashtag")
-	public List<cateData> cateData(){
+	public String hashRecy(String data, String method){
 		
-		List<String> cateList = searchRepo.findTrashNames();
-
-		List<cateData> cateNameList = new ArrayList<>();
-
-		ObjectMapper objectMapper = new ObjectMapper();
-
-		for (String data : cateList) {
-			String trashName = (String) data;
-
-
-			cateData cateData = new cateData();
-			cateData.setTrashName(trashName);
-
-			cateNameList.add(cateData);
-
+		Tb_Recycling hashRecy = searchRepo.findByTrashNameAndSearchMethod(data, method);
+		System.out.println(hashRecy + "===================================================================");
+		if(hashRecy!=null) {
+			ObjectMapper objectMapper = new ObjectMapper(); // json 변환
+			try {
+				String hashRecyData = objectMapper.writeValueAsString(hashRecy); // String으로 변환 
+				System.out.println(hashRecyData + "============================================================================================");
+				return hashRecyData;
+				
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
-		//	System.out.println(cateNameList.get(0).toString());
-
-		return cateNameList;
-	}
 		
-		
-	}
+		return "오류나면...옴";
+	}	
+}
 	
 	
 	
