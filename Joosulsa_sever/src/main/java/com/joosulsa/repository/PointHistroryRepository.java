eeeee9197ece3437db741e83ed3cc87feb5ac732
fpdls2789 <fpdls2789@gmail.com>
2024-01-11 09:40:01 +0900

    .

--- Joosulsa_sever/src/main/java/com/joosulsa/repository/PointHistroryRepository.java
+++ Joosulsa_sever/src/main/java/com/joosulsa/repository/PointHistroryRepository.java
@@ -9,7 +9,7 @@ import com.joosulsa.entity.Tb_Product;
 
 public interface PointHistroryRepository extends JpaRepository<Tb_Point_History, String>{
 	
-	@Query("SELECT SUM(use_point) FROM Tb_point_history WHERE user_id_user_id = :user_id_user_id")
+	@Query("SELECT SUM(usePoint) FROM Tb_Point_History WHERE userId.userId = :user_id_user_id")
 	int usePoint(@Param("user_id_user_id") String user_id_user_id);
 	
 	
